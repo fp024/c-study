6561fe38f49b29eec63d42ba447d3824c250d8fd
fp024 <aopenasus@naver.com>
2021-12-04 19:38:40 +0900

    release 모드로도 동작확인.
    release 모드로 빌드하면 4바이트 크기로만 할당이되고 순서대로 배치되었다.
    
    그런데, 주소를 바꾼후에 변수를 할당하는 동작이 안된다. ㅠㅠ 이 원인을 지금은 잘 모르겠음.

--- AddressDistance/main.c
+++ AddressDistance/main.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 
+/*
+ * https://stackoverflow.com/questions/8587764/remove-runtime-checks-compiler-flag-per-project-in-cmake
+ * https://over-stack.tistory.com/18
+ */
 int main() {
   int C1, C2;
   printf("&C1: %x\n", &C1);
   printf("&C2: %x\n", &C2);
 
-  int distance = (&C1 - &C2);
-  printf("distance : %d\n", distance);
+  int distance = &C1 - &C2;
+  int absDistance = abs(distance);
+  printf("distance : %d, absDistance: %d\n", distance, absDistance);
 
   int *ptrC1 = &C1;
-  for (int i = 0; i < distance; i++) {
-    ptrC1--;
+  for (int i = 0; i < absDistance; i++) {
+    if (distance > 0) {
+      ptrC1--;
+      printf("-ptrC1: %x\n",ptrC1);
+    } else if(distance < 0) {
+      ptrC1++;
+      printf("+ptrC1: %x\n",ptrC1);
+    }
   }
 
-  *ptrC1 = 100;
+  (*ptrC1) = 150;  // TODO: RELEASE 모드로 빌드시 주소가 정확하게 C2를 가리키는대도 할당이 안되는 이유를 모르겠음 ㅠㅠ
 
   printf("C2: %d\n", C2);
-
-  int a[2];
-  printf("&a0: %x\n", &a[0]);
-  printf("&a1: %x\n", &a[1]);
 
   return 0;
 }
