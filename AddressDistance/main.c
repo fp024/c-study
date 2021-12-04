4c0e8782bdaa2aa25c331a899dd16476cef54602
fp024 <aopenasus@naver.com>
2021-12-04 09:37:14 +0900

    디렉토리 이름을 잘못 적은 부분 수정.

--- AddressDistance/main.c
+++ AddressDistance/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    char a[10];
-    char b[10];
-    char c[10];
-    printf("한글a:   "); scanf("%s", a);
-    printf("한글b:   "); scanf("%s", b);
-    printf("한글c:   "); scanf("%s", c);
+  int C1, C2;
+  printf("&C1: %x\n", &C1);
+  printf("&C2: %x\n", &C2);
 
-    return 0;
+  int distance = (&C1 - &C2);
+  printf("distance : %d\n", distance);
+
+  int *ptrC1 = &C1;
+  for (int i = 0; i < distance; i++) {
+    ptrC1--;
+  }
+
+  *ptrC1 = 100;
+
+  printf("C2: %d\n", C2);
+
+  int a[2];
+  printf("&a0: %x\n", &a[0]);
+  printf("&a1: %x\n", &a[1]);
+
+  return 0;
 }
--- CLionScanf/main.c
+++ CLionScanf/main.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  int C1, C2;
-  printf("&C1: %x\n", &C1);
-  printf("&C2: %x\n", &C2);
+    char a[10];
+    char b[10];
+    char c[10];
+    printf("한글a:   "); scanf("%s", a);
+    printf("한글b:   "); scanf("%s", b);
+    printf("한글c:   "); scanf("%s", c);
 
-  int distance = (&C1 - &C2);
-  printf("distance : %d\n", distance);
-
-  int *ptrC1 = &C1;
-  for (int i = 0; i < distance; i++) {
-    ptrC1--;
-  }
-
-  *ptrC1 = 100;
-
-  printf("C2: %d\n", C2);
-
-  int a[2];
-  printf("&a0: %x\n", &a[0]);
-  printf("&a1: %x\n", &a[1]);
-
-  return 0;
+    return 0;
 }
